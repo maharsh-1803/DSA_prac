a0878458b820e786ae715ab6229e8a3f12f37f43
maharsh-1803 <maharshpatel1803@gmail.com>
2024-04-08 22:07:21 +0530

    hashing

--- src/hashing.java
+++ src/hashing.java
@@ -1,9 +1,14 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class hashing {
     public static void main(String[] args) {
-        int [] arr = {1,2,1,1,2};
+        int [] arr = {1,2,1,2};
         findFreq(arr,1);
+        String name="maharsh";
+        char target='a';
+        findCharFreq(name,target);
+        findFreqMap(arr,1);
     }
     public static void findFreq(int [] arr,int target)
     {
@@ -13,6 +18,25 @@ public class hashing {
             hash[arr[i]]++;
         }
         System.out.println(hash[target]);
+
+    }
+
+    public static void findCharFreq(String s,char target)
+    {
+        int [] hash = new int[256];
+        Arrays.fill(hash,0);
+        for (int i = 0; i <s.length() ; i++) {
+            hash[s.charAt(i)]++;
+        }
+        System.out.println(hash[target]);
+    }
+    public static void findFreqMap(int [] arr,int target)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i <arr.length ; i++) {
+            map.put(arr[i],i);
+        }
+        System.out.println(map.get(target));
 
     }
 }
